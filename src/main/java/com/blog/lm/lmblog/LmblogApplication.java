8978ab283da42f36d192512193f0ff0f8762c3bf
xusheng <scaf_xs@163.com>
2019-11-04 18:41:20 +0800

    提交：提交文件

---
+++ src/main/java/com/blog/lm/lmblog/LmblogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.lm.lmblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmblogApplication.class, args);
+    }
+
+}
