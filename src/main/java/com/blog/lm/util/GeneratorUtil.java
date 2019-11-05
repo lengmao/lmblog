8f56ded077bfd5002ea40d094e68826d25bb3e8e
xus <scaf_xs@163.com>
2019-11-05 22:50:39 +0800

    添加：添加代码自动生成工具

---
+++ src/main/java/com/blog/lm/util/GeneratorUtil.java
@@ -0,0 +1,73 @@
+package com.blog.lm.util;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+
+public class GeneratorUtil {
+    private static final String targetDir = "E:\\generator";
+    private static final String driverName = "com.mysql.cj.jdbc.Driver";
+    private static final String url = "jdbc:mysql://xxxx.xxxx.xxx.xxxx:33306/lmblog?useUnicode=true&useSSL=false&characterEncoding=utf8";
+    private static final String username = "root";
+    private static final String password = "123456";
+    private static String[] tables = new String[]{"sys_user"};
+
+    public static void main(String[] args) {
+        //全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setOutputDir(targetDir);
+        gc.setFileOverride(true);
+        gc.setActiveRecord(false);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+        gc.setAuthor("xus");// 作者
+
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setControllerName("%sController");
+        gc.setServiceName("%sService");
+        gc.setServiceImplName("%sServiceImpl");
+        gc.setMapperName("%sMapper");
+        gc.setXmlName("%sMapper");
+
+
+        //数据源配置
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setDriverName(driverName)
+                .setUrl(url)
+                .setUsername(username)
+                .setPassword(password);
+        //策略配置
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setCapitalMode(true)
+                //.setDbColumnUnderline(true)
+                .setEntityLombokModel(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setInclude(tables); // 生成的表,可同时传入多个表名
+        //包名策略配置
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent("com.blog.lm")
+                .setMapper("mapper")
+                .setService("service")
+                .setController("controller")
+                .setEntity("entity")
+                .setXml("mapper");
+
+        //整合配置
+        AutoGenerator ag = new AutoGenerator();
+        ag.setGlobalConfig(gc)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(packageConfig);
+
+        //执行
+        ag.execute();
+    }
+
+}
