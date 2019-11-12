26f5764f0f3009d82df33f61c0a5677a39990ca2
cwm <cwm3412638@163.com>
2019-11-12 14:59:11 +0800

    添加上传文件到服务器操作

--- src/main/java/com/blog/lm/LmblogApplication.java
+++ src/main/java/com/blog/lm/LmblogApplication.java
@@ -2,9 +2,11 @@ package com.blog.lm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 public class LmblogApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/blog/lm/upload/controller/UploadContrller.java
@@ -0,0 +1,78 @@
+package com.blog.lm.upload.controller;
+
+import cn.hutool.core.io.FileUtil;
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.domain.proto.storage.DownloadByteArray;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 操作图片服务器接口
+ */
+@RestController
+@RequestMapping("/upload")
+public class UploadContrller {
+    @Resource
+    private FastFileStorageClient fastFileStorageClient;
+
+    /**
+     * 上传接口
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    @PostMapping("/upload")
+    public String save(MultipartFile file) throws IOException {
+        byte[] bytes = file.getBytes();
+        InputStream inputStream = new ByteArrayInputStream(bytes);
+        String extName = FileUtil.extName(file.getOriginalFilename());
+        StorePath storePath = fastFileStorageClient.uploadFile(inputStream, bytes.length, extName, null);
+        return storePath.getFullPath();
+    }
+
+    /**
+     * 删除接口
+     * @param uri
+     * @return
+     */
+    @PostMapping("/del")
+    public String del(@RequestParam String uri) {
+        StorePath storePath = fromUri(uri);
+        fastFileStorageClient.deleteFile(storePath.getGroup(), storePath.getPath());
+        return "删除成功";
+    }
+    /**
+     * 从uri生成StorePath对象.
+     */
+    private StorePath fromUri(String uri) {
+        String group = uri.replaceFirst("/.*$", "");
+        String path = uri.replaceFirst("^.*?/", "");
+        return new StorePath(group, path);
+    }
+
+    /**
+     * 下载
+     * @param fullPath
+     * @return
+     */
+    public byte[] getBin(String fullPath) {
+        try {
+            StorePath storePath = fromUri(fullPath);
+            String group = storePath.getGroup();
+            String path = storePath.getPath();
+            return fastFileStorageClient.downloadFile(group, path, new DownloadByteArray());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
